02bad3beb4a0a6764777d7725ee0704763e9677f
VladislavFl <vlad23fleganov@gmail.com>
2023-05-20 22:49:50 +0300

    Admin panel, update user by admin, different styles depends on the role

--- ProjectManagementSystem/Program.cs
+++ ProjectManagementSystem/Program.cs
@@ -51,6 +51,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Account}/{action=Login}/{id?}");
 
 app.Run();
